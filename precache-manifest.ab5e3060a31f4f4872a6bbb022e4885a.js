8d6f73fe605f35a6fa8b714ad7c2cafb4ef47299
mobarokman <mobarokman@gmail.com>
2019-12-20 12:27:08 +0600

    added build in master

---
+++ precache-manifest.ab5e3060a31f4f4872a6bbb022e4885a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2fd7336faf66363ec49f69a49dbfed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4798e37cc563d5d4e8c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a563775a6348cd890ff8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4798e37cc563d5d4e8c",
+    "url": "/static/js/2.dae4d7ed.chunk.js"
+  },
+  {
+    "revision": "97016f7c8eabfcd85cb83217877537cf",
+    "url": "/static/js/2.dae4d7ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a563775a6348cd890ff8",
+    "url": "/static/js/main.461ede2f.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
