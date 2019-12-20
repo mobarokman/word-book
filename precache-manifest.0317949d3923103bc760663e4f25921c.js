7242148c212be7f532c53b1b106e209b1ad6b8e5
mobarokman <mobarokman@gmail.com>
2019-12-20 18:29:35 +0600

    Updates

---
+++ precache-manifest.0317949d3923103bc760663e4f25921c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784873bcf2b28918d826f5f99eab3759",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "c61dc77df23a16eee904",
+    "url": "/notebook/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5646cc3db0813c6ad118",
+    "url": "/notebook/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c61dc77df23a16eee904",
+    "url": "/notebook/static/js/2.85521435.chunk.js"
+  },
+  {
+    "revision": "18db333adb41a97042cb1281e6b75be4",
+    "url": "/notebook/static/js/2.85521435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5646cc3db0813c6ad118",
+    "url": "/notebook/static/js/main.2a33a981.chunk.js"
+  },
+  {
+    "revision": "d57059ac1d437d267455",
+    "url": "/notebook/static/js/runtime-main.30557a2e.js"
+  }
+]);
